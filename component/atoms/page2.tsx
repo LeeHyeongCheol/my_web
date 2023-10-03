662e30c0822ff2505b84b8e6974b8a86e78100e0
LeeHyeongCheol <r7042@naver.com>
2023-10-03 22:12:06 +0900

    width조정

--- component/atoms/page2.tsx
+++ component/atoms/page2.tsx
@@ -10,14 +10,10 @@ export default function Page2() {
           <h1 className={styles.title}>Hi, I’m Lee Hyeongcheol. Nice to meet you.</h1>
           <h2 className={styles.subtitle}>
             「Front-end Engineer」としてデザイナーと企画者と協力してデザインと機能をウェブに実装したりとか多様な顧客のニーズを満たし、
-            <br />
             エラーを予防できるようウェブを実装するために努力しています。主に「React.js、Next.js、TypeScript」を活用してウェブを実装していますが、
-            <br />
             Next.js を活用して APIRoutes および SEO サポート、イメージ処理などをして、 React.js では Redux を活用して全域の状態管理および
-            <br />
             TypeScript を通じて合わない TYPE や関数のエラーを事前に予防しています。そして Chrome、Safari、Edge およびモバイル画面(iphone、Galaxy)も同様に実行されるようにResponsive を実装しています。
-            <br />
-          </h2>
+           </h2>
           <Image src="" alt="" />
         </div>
       </div>
